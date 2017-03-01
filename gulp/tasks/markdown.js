b02765cabe449594ed1c57663c62611f3394bb36
Jan Rembold <jan.rembold@virtual-identity.com>
2017-03-01 13:41:37 +0100

    added wrapper for markdown task

--- gulp/tasks/markdown.js
+++ gulp/tasks/markdown.js
@@ -1,15 +1,17 @@
 var gulp = require('gulp');
 var markdown = require('gulp-markdown');
+var wrap = require('gulp-wrap');
 var gutil = require('gulp-util');
 var config = require('./../config');
 
 gulp.task('markdown', function () {
 
-	if (config.global.tasks.markdown) {
-		return gulp.src(config.global.docs + '/_src/**/*.md')
-			.pipe(markdown(config.markdown))
-			.pipe();
-	} else {
-		gutil.log(gutil.colors.yellow('markdown disabled'));
-	}
+    if (config.global.tasks.markdown) {
+        return gulp.src('./readme.md')
+            .pipe(markdown(config.markdown))
+            .pipe(wrap({ src: config.global.docs + '/resources/wraps/markdown.tpl.html' }))
+            .pipe(gulp.dest(config.global.docs));
+    } else {
+        gutil.log(gutil.colors.yellow('markdown disabled'));
+    }
 });
