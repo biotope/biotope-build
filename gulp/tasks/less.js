b374d52da7172c1d104222d8b0a63cc7c1f13aff
Clemens Fiedler <clemens.fiedler@virtual-identity.com>
2017-03-07 16:07:52 +0100

    added gulp-less task including gulp-less-reporter for error reporting

---
+++ gulp/tasks/less.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var lessReporter = require('gulp-less-reporter');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cached = require('gulp-cached');
+var watch = require('gulp-watch');
+var runSequence = require('run-sequence');
+var mergeStream = require('merge-stream');
+var config = require('./../config');
+var sourcemaps = require('gulp-sourcemaps');
+
+
+gulp.task('less', function () {
+
+	return mergeStream(config.global.resources.map( function(currentResource) {
+		return gulp.src([
+			config.global.src + currentResource + '/less/**/*.less',
+			'!' + config.global.src + currentResource + '/less/**/_*.less'
+		])
+			.pipe(sourcemaps.init())
+			.pipe(less(config.less).on('error', lessReporter))
+			.pipe(postcss([
+				autoprefixer(config.autoprefixer)
+			]))
+			.pipe(sourcemaps.write('.'))
+			.pipe(gulp.dest(config.global.dev + currentResource + '/css'));
+	}));
+});
+
+gulp.task('watch:less', function () {
+
+	config.global.resources.forEach(function(currentResource) {
+		watch([
+			config.global.src + currentResource + '/css/**/*.less'
+		], function () {
+			runSequence(
+				['lint:less'],
+				['less']
+			);
+		});
+	});
+
+});
