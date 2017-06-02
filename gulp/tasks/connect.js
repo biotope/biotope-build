6c6f488412580a9c62cc98b9808c190860401de0
Jan Rembold <janrembold@googlemail.com>
2017-06-02 23:14:03 +0200

    reconfigured connect task

--- gulp/tasks/connect.js
+++ gulp/tasks/connect.js
@@ -5,36 +5,34 @@ var cached = require('gulp-cached');
 var watch = require('gulp-watch');
 var config = require('./../config');
 
+gulp.task('livereload:init', function () {
+
+    return gulp.src(config.connect.globs)
+        .pipe(cached('livereload'));
+
+});
 
 gulp.task('livereload', function () {
 
-    return watch([
-        config.global.dev + '/**/*',
-        config.global.src + '/resources/js/**/*.js',
-        config.global.src + '/resources/bower_components/**/*',
-        config.global.src + '/_mock/**/*',
-        config.global.src + '/_assets/**/*',
-        '!' + config.global.dev + '/_mock/**/*',
-        '!' + config.global.dev + '/_assets/**/*',
-        '!' + config.global.dev + '/resources/js/vendor/**/*.js',
-        '!' + config.global.dev + '/resources/bower_components/**/*',
-        '!' + config.global.dev + '/resources/js/handlebars.templates.js'
-    ], { ignoreInitial: true })
+    watch(config.connect.globs)
         .pipe(cached('livereload'))
-        .pipe(gulpLivereload());
+        .pipe(connect.reload());
 
 });
 
 gulp.task('connect:open', function () {
 
-	return opn('http://localhost:' + config.connect.port);
+	opn('http://localhost:' + config.connect.port);
 
 });
 
 gulp.task('connect', function () {
 
     connect.server({
-        root: [config.global.dev, config.global.src],
+        root: [
+            config.global.dev,
+            config.global.src
+        ],
 		port: config.connect.port,
         livereload: config.livereload
     });
