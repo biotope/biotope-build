8756d910be8ad079582e34a50fc5ff91c0105a2e
Jan Rembold <jan.rembold@virtual-identity.com>
2017-12-21 23:10:41 +0100

    added lazy loading to cleanCss task

--- tasks/cleanCss.js
+++ tasks/cleanCss.js
@@ -1,51 +1,46 @@
-const gulp = require('gulp');
-const size = require('gulp-size');
-const cleanCss = require('gulp-clean-css');
-const mergeStream = require('merge-stream');
-const colors = require('colors/safe');
-
 const config = require('./../config');
+const $ = config.plugins;
 
-gulp.task('cleanCss:resources:dist', function () {
+$.gulp.task('cleanCss:resources:dist', function () {
 
 	if (!config.global.tasks.cleanCss) {
-		console.log(colors.yellow('cleanCss disabled'));
+		console.log($.colors.yellow('cleanCss disabled'));
 	}
 
-	return mergeStream(config.global.resources.map(function (currentResource) {
-		const stream = gulp.src(config.global.dev + currentResource + '/css/**/*.css');
+	return $.mergeStream(config.global.resources.map(function (currentResource) {
+		const stream = $.gulp.src(config.global.dev + currentResource + '/css/**/*.css');
 
 		if (config.global.tasks.cleanCss) {
-			stream.pipe(cleanCss(config.cleanCss))
-				.pipe(size({
+			stream.pipe($.cleanCss(config.cleanCss))
+				.pipe($.size({
 					title: 'minified',
 					showFiles: true
 				}));
 		}
 
-		stream.pipe(gulp.dest(config.global.dist + currentResource + '/css/'));
+		stream.pipe($.gulp.dest(config.global.dist + currentResource + '/css/'));
 		return stream;
 	}));
 });
 
-gulp.task('cleanCss:components:dist', function () {
+$.gulp.task('cleanCss:components:dist', function () {
 
 	if (!config.global.tasks.cleanCss) {
-		console.log(colors.yellow('cleanCss disabled'));
+		console.log($.colors.yellow('cleanCss disabled'));
 	}
 
-	return mergeStream(config.global.resources.map(function (currentResource, index) {
-		const stream = gulp.src(config.global.dev + currentResource + config.global.components[index] + '/**/*.css');
+	return $.mergeStream(config.global.resources.map(function (currentResource, index) {
+		const stream = $.gulp.src(config.global.dev + currentResource + config.global.components[index] + '/**/*.css');
 
 		if (config.global.tasks.cleanCss) {
-			stream.pipe(cleanCss(config.cleanCss))
-				.pipe(size({
+			stream.pipe($.cleanCss(config.cleanCss))
+				.pipe($.size({
 					title: 'minified',
 					showFiles: true
 				}));
 		}
 
-		stream.pipe(gulp.dest(config.global.dist + currentResource + config.global.components[index]));
+		stream.pipe($.gulp.dest(config.global.dist + currentResource + config.global.components[index]));
 		return stream;
 	}));
 });
