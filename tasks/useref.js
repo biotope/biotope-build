e69deb95e22bfdb13f8d34ed8b650ed82a711f35
Jan Rembold <jan.rembold@virtual-identity.com>
2017-12-22 13:40:02 +0100

    added lazy loading to useref task

--- tasks/useref.js
+++ tasks/useref.js
@@ -1,30 +1,23 @@
-const gulp = require('gulp');
-const useref = require('gulp-useref');
-const hb = require('gulp-hb');
-const filter = require('gulp-filter');
-const uglify = require('gulp-uglify');
-const cleanCss = require('gulp-clean-css');
-const lec = require('gulp-line-ending-corrector');
-
-const hbsParser = require('./../lib/hbs-parser');
 const config = require('./../config');
+const $ = config.plugins;
 
-gulp.task('useref', function () {
+$.gulp.task('useref', function () {
 
-	return gulp.src(config.global.dev + '/*.html')
-		.pipe(lec(config.lec))
-		.pipe(useref({
+	return $.gulp.src(config.global.dev + '/*.html')
+		.pipe($.lineEndingCorrector(config.lec))
+		.pipe($.useref({
 			noAssets: true
 		}))
-		.pipe(filter(['**/*.html']))
-		.pipe(gulp.dest(config.global.dist));
+		.pipe($.filter(['**/*.html']))
+		.pipe($.gulp.dest(config.global.dist));
 
 });
 
-gulp.task('useref:assets', function () {
+$.gulp.task('useref:assets', function () {
 
-	const jsFilter = filter(['**/*.js'], {restore: true});
-	const cssFilter = filter(['**/*.css'], {restore: true});
+	const hbsParser = require('./../lib/hbs-parser');
+	const jsFilter = $.filter(['**/*.js'], {restore: true});
+	const cssFilter = $.filter(['**/*.css'], {restore: true});
 
 	let hbStream = hbsParser.createHbsGulpStream(
 		[
@@ -34,20 +27,20 @@ gulp.task('useref:assets', function () {
 		null, null, config.global.debug
 	);
 
-	return gulp.src(config.global.src + '/resources/_useref.html')
-		.pipe(lec(config.lec))
+	return $.gulp.src(config.global.src + '/resources/_useref.html')
+		.pipe($.lineEndingCorrector(config.lec))
 		.pipe(hbStream)
-		.pipe(useref())
+		.pipe($.useref())
 
 		.pipe(jsFilter)
-		.pipe(uglify(config.uglify))
+		.pipe($.uglify(config.uglify))
 		.pipe(jsFilter.restore)
 
 		.pipe(cssFilter)
-		.pipe(cleanCss(config.cleanCss))
+		.pipe($.cleanCss(config.cleanCss))
 		.pipe(cssFilter.restore)
 
-		.pipe(filter(['**', '!**/_useref.html']))
+		.pipe($.filter(['**', '!**/_useref.html']))
 
-		.pipe(gulp.dest(config.global.dist));
+		.pipe($.gulp.dest(config.global.dist));
 });
