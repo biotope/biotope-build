2306fb2d39f9addeef63e6c73ed56095545164f1
Jan Rembold <jan.rembold@virtual-identity.com>
2017-12-22 11:01:17 +0100

    added lazy loading to inject task

--- tasks/inject.js
+++ tasks/inject.js
@@ -1,17 +1,16 @@
-const gulp = require('gulp');
-const inject = require('gulp-inject');
 const config = require('./../config');
+const $ = config.plugins;
 
-gulp.task('inject', function () {
+$.gulp.task('inject', function () {
 	if (config.global.tasks.favicons) {
-		return gulp.src(config.global.dist + '/*.html')
-			.pipe(inject(gulp.src([config.global.dist + '/favicons/htmlhead.favicons.html']), {
+		return $.gulp.src(config.global.dist + '/*.html')
+			.pipe($.inject($.gulp.src([config.global.dist + '/favicons/htmlhead.favicons.html']), {
 				starttag: '<!-- inject:head:{{ext}} -->',
 				transform: function (filePath, file) {
 					// return file contents as string
 					return file.contents.toString('utf8')
 				}
 			}))
-			.pipe(gulp.dest(config.global.dist));
+			.pipe($.gulp.dest(config.global.dist));
 	}
 });
