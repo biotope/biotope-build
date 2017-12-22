04cf283c065cae804861cacbda4c608adbda456d
Jan Rembold <jan.rembold@virtual-identity.com>
2017-12-22 11:58:32 +0100

    added lazy loading to json task

--- tasks/json.js
+++ tasks/json.js
@@ -1,31 +1,25 @@
-const gulp = require('gulp');
-const colors = require('colors/safe');
-const jsonlint = require('gulp-jsonlint');
-const cached = require('gulp-cached');
-const watch = require('gulp-watch');
-const runSequence = require('run-sequence');
-
 const config = require('./../config');
+const $ = config.plugins;
 
-gulp.task('lint:json', function () {
+$.gulp.task('lint:json', function () {
 
 	if (config.global.tasks.linting) {
-		return gulp.src(config.global.src + "/**/*.json")
-			.pipe(cached('json', { optimizeMemory: true }))
-			.pipe(jsonlint())
-			.pipe(jsonlint.reporter());
+		return $.gulp.src(config.global.src + "/**/*.json")
+			.pipe($.cached('json', { optimizeMemory: true }))
+			.pipe($.jsonlint())
+			.pipe($.jsonlint.reporter());
 	} else {
-		console.log(colors.yellow('linting json disabled'));
+		console.log($.colors.yellow('linting json disabled'));
 	}
 });
 
-gulp.task('watch:json', function () {
+$.gulp.task('watch:json', function () {
 
 	if (config.global.tasks.linting) {
-		watch([
+		$.watch([
 			config.global.src + '/**/*.json'
 		], config.watch, function () {
-			runSequence('lint:json');
+			$.runSequence('lint:json');
 		});
 	}
 });
