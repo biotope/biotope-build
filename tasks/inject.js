911c58f7767f1b57e476b7baf90dc966e29a2a62
Jan Rembold <jan.rembold@virtual-identity.com>
2017-12-26 15:40:06 +0100

    refactored source and target paths to use path.join

--- tasks/inject.js
+++ tasks/inject.js
@@ -1,16 +1,22 @@
+const path = require('path');
 const config = require('./../config');
 const $ = config.plugins;
 
 $.gulp.task('inject', function () {
 	if (config.global.tasks.favicons) {
-		return $.gulp.src(config.global.dist + '/*.html')
-			.pipe($.inject($.gulp.src([config.global.dist + '/favicons/htmlhead.favicons.html']), {
+
+		const htmlSourcePaths = path.join(config.global.cwd, config.global.dist, '*.html');
+		const favionsSourcePaths = path.join(config.global.cwd, config.global.dist, 'favicons', 'htmlhead.favicons.html');
+		const targetPath = path.join(config.global.cwd, config.global.dist);
+
+		return $.gulp.src(htmlSourcePaths)
+			.pipe($.inject($.gulp.src(favionsSourcePaths), {
 				starttag: '<!-- inject:head:{{ext}} -->',
 				transform: function (filePath, file) {
 					// return file contents as string
 					return file.contents.toString('utf8')
 				}
 			}))
-			.pipe($.gulp.dest(config.global.dist));
+			.pipe($.gulp.dest(targetPath));
 	}
 });
