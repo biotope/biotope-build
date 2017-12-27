a6c981c2e9bb1e578996b993d8d5b4fbc045374d
Jan Rembold <jan.rembold@virtual-identity.com>
2017-12-27 11:59:27 +0100

    refactored source and target paths to use path.join

--- tasks/modernizr.js
+++ tasks/modernizr.js
@@ -1,18 +1,21 @@
+const path = require('path');
 const config = require('./../config');
 const $ = config.plugins;
 
 $.gulp.task('modernizr', function () {
 
-	return $.gulp.src([
-			config.global.src + '/resources/js/**/*.js',
-            config.global.src + '/resources/ts/**/*.+(t|j)s',
-            config.global.src + '/resources/react/**/*.+(j|t)sx',
-			config.global.src + '/_mock/configuration.js',
-			config.global.dev + '/resources/css/**/*.css',
-			'!' + config.global.src + '/resources/js/vendor/*'
-		])
+	const sourcePaths = [
+		path.join(config.global.cwd, config.global.src, 'resources', 'js', '**', '*.js'),
+		path.join(config.global.cwd, config.global.src, 'resources', 'ts', '**', '*.+(t|j)s'),
+		path.join(config.global.cwd, config.global.src, 'resources', 'react', '**', '*.+(j|t)sx'),
+		path.join(config.global.cwd, config.global.src, '_mock', 'configuration.js'),
+		path.join(config.global.cwd, config.global.dev, 'resources', 'css', '**', '*.css'),
+		'!' + path.join(config.global.cwd, config.global.src, 'resources', 'js', 'vendor', '**', '*')
+	];
+	const targetPath = path.join(config.global.cwd, config.global.dev, 'resources', 'js', 'vendor');
+
+	return $.gulp.src(sourcePaths)
 		.pipe($.modernizr(config.modernizr))
 		.pipe($.uglify(config.uglify))
-		.pipe($.gulp.dest(config.global.dev + '/resources/js/vendor/'));
-
+		.pipe($.gulp.dest(targetPath));
 });
