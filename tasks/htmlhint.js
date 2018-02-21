5cfca93b66938a5b2382aee8141c93abf86371cc
Jan Rembold <jan.rembold@virtual-identity.com>
2018-02-21 16:34:38 +0100

    moved requires into functions

--- tasks/htmlhint.js
+++ tasks/htmlhint.js
@@ -1,12 +1,9 @@
 const gulp = require('gulp');
-const htmlhint = require('gulp-htmlhint');
-const cached = require('gulp-cached');
-const watch = require('gulp-watch');
-const runSequence = require('run-sequence');
-
 const config = require('./../config');
 
 gulp.task('htmlhint', function () {
+	const htmlhint = require('gulp-htmlhint');
+	const cached = require('gulp-cached');
 
 	return gulp.src(config.global.dev + '/*.html')
 		.pipe(cached('htmlhint', { optimizeMemory: true }))
@@ -16,6 +13,8 @@ gulp.task('htmlhint', function () {
 });
 
 gulp.task('watch:html', function () {
+	const watch = require('gulp-watch');
+	const runSequence = require('run-sequence');
 
 	watch([
 		config.global.dev + '/*.html'
