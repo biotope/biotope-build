d840ad32c32a6f71bebb314b32fe7136a546d6d1
Saeid Mohadjer <saeid@fastmail.fm>
2018-02-23 10:58:34 +0100

    implements feedback from jan

--- tasks/browserSupport.js
+++ tasks/browserSupport.js
@@ -2,46 +2,50 @@ const gulp = require('gulp');
 const config = require('./../config');
 
 gulp.task('browserSupport', function () {
-	const jsonParser = require('../lib/json-parser');
-	const browserSupportData = jsonParser.getBrowserSupportData();
 	const colors = require('colors/safe');
 
-	if (config.global.tasks.browserSupport && browserSupportData) {
-		const rename = require('gulp-rename');
+	if (config.global.tasks.browserSupport) {
+		const jsonParser = require('../lib/json-parser');
+		const browserSupportData = jsonParser.getBrowserSupportData();
 
-		const dataObject = {
-			package: require(config.global.cwd + '/package.json'),
-			browserSupport: browserSupportData
-		};
+		if (browserSupportData) {
+			const rename = require('gulp-rename');
+			const dataObject = {
+				package: require(config.global.cwd + '/package.json'),
+				browserSupport: browserSupportData
+			};
 
-		if (config.global.debug) {
-			console.log(colors.green(`dataObject: ${JSON.stringify(dataObject)}`));
+			if (config.global.debug) {
+				console.log(colors.green(`dataObject: ${JSON.stringify(dataObject)}`));
+			}
+
+			const hbsParser = require('./../lib/hbs-parser');
+			const hbStream = hbsParser.createHbsGulpStream(null, dataObject, null, config.global.debug);
+
+			return gulp.src(config.global.src + '/browserSupport.hbs')
+				.pipe(hbStream)
+				.pipe(rename({extname: ".html"}))
+				.pipe(gulp.dest(config.global.dev));
 		}
-
-		const hbsParser = require('./../lib/hbs-parser');
-		const hbStream = hbsParser.createHbsGulpStream(null, dataObject, null, config.global.debug);
-
-		return gulp.src(config.global.src + '/browserSupport.hbs')
-			.pipe(hbStream)
-			.pipe(rename({extname: ".html"}))
-			.pipe(gulp.dest(config.global.dev));
 	} else {
 		console.log(colors.yellow('browserSupport disabled'));
 	}
 });
 
 gulp.task('watch:browserSupport', function () {
-	const jsonParser = require('../lib/json-parser');
-	const browserSupportData = jsonParser.getBrowserSupportData();
-	
-	if (config.global.tasks.browserSupport && browserSupportData) {
-		const watch = require('gulp-watch');
-		const runSequence = require('run-sequence');
+	if (config.global.tasks.browserSupport) {
+		const jsonParser = require('../lib/json-parser');
+		const browserSupportData = jsonParser.getBrowserSupportData();
 
-		watch(config.global.cwd + './browserSupport.json', config.watch, function () {
-			runSequence(
-				['browserSupport']
-			);
-		});
+		if (browserSupportData) {
+			const watch = require('gulp-watch');
+			const runSequence = require('run-sequence');
+
+			watch(config.global.cwd + './browserSupport.json', config.watch, function () {
+				runSequence(
+					['browserSupport']
+				);
+			});
+		}
 	}
 });
