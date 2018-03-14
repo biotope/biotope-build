50518a66c647c9941dae1c3a7717ea8eeff96713
Saeid Mohadjer <saeid@fastmail.fm>
2018-03-14 09:05:32 +0100

    Adds a new gulp task to copy component mock folder

--- index.js
+++ index.js
@@ -75,6 +75,7 @@ gulp.task('build', function (callback) {
 			'copy:dist:assets',
 			'copy:dist:css',
 			'copy:dist:mock',
+			'copy:dist:component:mock',
 			'copy:dist:config',
 			'copy:dist:hbs',
 			'copy:dist:bower'
