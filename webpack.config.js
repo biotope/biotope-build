71c30a16b16ac901ae54444c766b05a143b77078
SheepFromHeaven <marcwissler@gmail.com>
2018-09-04 15:31:40 +0200

    Enable loose class compilation

--- webpack.config.js
+++ webpack.config.js
@@ -71,8 +71,8 @@ module.exports = {
             loader: 'babel-loader',
             options: {
               babelrc: false,
-              presets: ['@babel/preset-env'],
-              plugins: [require('@babel/plugin-transform-classes').default]
+              presets: [['@babel/preset-env']],
+              plugins: [[require('@babel/plugin-transform-classes').default, { loose: true }]]
             }
           }
         ]
