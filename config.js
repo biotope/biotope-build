3a7fd8ebee58ed75fbe475bc4f0f5d29200d9c04
Marc Emmanuel <marcwissler@gmail.com>
2018-11-23 13:59:15 +0100

    Add possibility to customize build entry points (#156)
    * Add possibility to customize build entry points
    
    * Add default config entry points

--- config.js
+++ config.js
@@ -36,6 +36,10 @@ module.exports = {
       version: true,
       webpack: true
     },
+    tsEntryPoints: [
+      'resources/ts/**/*.ts',
+      'components/**/*.ts'
+    ],
     externalResources: {},
     bowerResources: {}
   },
