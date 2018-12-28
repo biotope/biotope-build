584a7f48011ed6575a524c6d6acfd9efa76b786c
Christoph Hummel <christoph.hummel@virtual-identity.com>
2018-12-28 14:42:05 +0100

    add global config for the style-guide task

--- tasks/styleGuide.js
+++ tasks/styleGuide.js
@@ -1,5 +1,9 @@
+const gulp = require('gulp');
+const config = require('./../config');
+
 gulp.task('styleGuide', function () {
   if (config.global.tasks.styleGuide) {
     const styleGuide = require('@biotope/styleguide/src/index');
+    styleGuide(config);
   }
 });
