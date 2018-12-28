19a765727e3759d004420bb9a9fa39c07db04aed
Christoph Hummel <christoph.hummel@virtual-identity.com>
2018-12-28 16:10:53 +0100

    add hbs

--- tasks/styleGuide.js
+++ tasks/styleGuide.js
@@ -1,9 +1,11 @@
 const gulp = require('gulp');
 const config = require('./../config');
+const Handlebars = require('handlebars');
+const hbsHelper = require('./../lib/hb2-helpers');
 
 gulp.task('styleGuide', function () {
   if (config.global.tasks.styleGuide) {
     const styleGuide = require('@biotope/styleguide/src/index');
-    styleGuide(config);
+    styleGuide(config, Handlebars, hbsHelper);
   }
 });
