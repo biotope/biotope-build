c5176b88cfcc03b0d4e545c0210884896391e171
Christoph Hummel <christoph.hummel@virtual-identity.com>
2019-01-14 13:49:45 +0100

    change required file for style-guide script

--- tasks/styleGuide.js
+++ tasks/styleGuide.js
@@ -5,7 +5,7 @@ const hbsHelper = require('./../lib/hb2-helpers');
 
 gulp.task('styleGuide', function () {
   if (config.global.tasks.styleGuide) {
-    const styleGuide = require('@biotope/styleguide/src/index');
+    const styleGuide = require('@biotope/styleguide/src/mergePackageJson');
     styleGuide(config, Handlebars, hbsHelper);
   }
 });
