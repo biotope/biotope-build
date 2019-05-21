351978527efaa78e17e34037ec08bf58d7a72d6e
Jurek Barth <jurek.barth@virtual-identity.com>
2019-05-21 17:17:25 +0200

    make htmlhint configurable

--- tasks/htmlhint.js
+++ tasks/htmlhint.js
@@ -1,22 +1,31 @@
 const gulp = require('gulp');
 const config = require('./../config');
+const colors = require('colors/safe');
 
-gulp.task('htmlhint', function() {
-  const htmlhint = require('gulp-htmlhint');
-  const cached = require('gulp-cached');
+gulp.task('htmlhint', function () {
+  if (config.global.tasks.htmlhint) {
+    const htmlhint = require('gulp-htmlhint');
+    const cached = require('gulp-cached');
 
-  return gulp
-    .src(config.global.dev + '/*.html')
-    .pipe(cached('htmlhint', { optimizeMemory: true }))
-    .pipe(htmlhint('.htmlhintrc'))
-    .pipe(htmlhint.reporter('htmlhint-stylish'));
+    return gulp
+      .src(config.global.dev + '/*.html')
+      .pipe(cached('htmlhint', { optimizeMemory: true }))
+      .pipe(htmlhint('.htmlhintrc'))
+      .pipe(htmlhint.reporter('htmlhint-stylish'));
+  } else {
+    console.log(colors.yellow('htmlhint disabled'));
+  }
 });
 
-gulp.task('watch:html', function() {
-  const watch = require('gulp-watch');
-  const runSequence = require('run-sequence');
+gulp.task('watch:html', function () {
+  if (config.global.tasks.htmlhint) {
+    const watch = require('gulp-watch');
+    const runSequence = require('run-sequence');
 
-  watch([config.global.dev + '/*.html'], config.watch, function() {
-    runSequence('htmlhint');
-  });
+    watch([config.global.dev + '/*.html'], config.watch, function () {
+      runSequence('htmlhint');
+    });
+  } else {
+    console.log(colors.yellow('htmlhint disabled'));
+  }
 });
