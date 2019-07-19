54b3ca2e9a30e20a8337e7d7bab516b3e4633821
Tiago Marques <tiago.marques@virtual-identity.com>
2019-07-19 17:28:30 +0200

    Minor lint

--- src/tasks/log-version.ts
+++ src/tasks/log-version.ts
@@ -1,11 +1,12 @@
-import * as log from 'console-emoji';
+import * as consoleEmoji from 'console-emoji';
 
 import { GulpTaskCallback } from '../types';
 
 export const logVersion: GulpTaskCallback = (resolve): void => {
   // eslint-disable-next-line import/no-dynamic-require,global-require
   const { version } = require(`${__dirname}/../../package.json`);
+  const loggedText = `:sparkles: Starting Biotope Build (v${version}) with :sparkling_heart: for Frontend Developers around the world :sparkles:`;
 
-  log(`:sparkles: Starting Biotope Build (v${version}) with :sparkling_heart: for Frontend Developers around the world :sparkles:\n`, 'green');
+  consoleEmoji(`${loggedText}\n`, 'green');
   resolve();
 };
