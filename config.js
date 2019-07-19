58c005a36e19ec628fda5ee0d03cfbddf5ab5849
VI\timo.mayer <timo.mayer@virtual-identity.com>
2019-07-20 00:54:36 +0200

    remove autoprefixer browser config option (replaced by browserlistrc file

--- config.js
+++ config.js
@@ -47,8 +47,6 @@ module.exports = {
 
   //=== Plugins ===//
   autoprefixer: {
-    //browsers: ['last 3 versions', 'last 8 Chrome versions', 'last 8 Firefox versions' , 'Firefox ESR', 'ie 9', 'last 2 iOS versions', 'Android 4']
-    browsers: ['last 1 version'],
     grid: true
   },
 
