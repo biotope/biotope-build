94fb44051ae7302c5c80bee2955d9c22b030cd5d
Tiago Marques <tiago.marques@virtual-identity.com>
2019-12-10 17:25:09 +0000

    Make dev-preview compatible with ie

--- plugins/dev-preview/files/dev-preview.js
+++ plugins/dev-preview/files/dev-preview.js
@@ -1,90 +1,91 @@
 /* eslint-disable */
-(async () => {
-  const Header = Vue.extend({
+(function() {
+  function fetchText(url) {
+    var request = new XMLHttpRequest();
+    request.open('GET', url, false);
+    request.send();
+    return request.status === 200 ? request.responseText : undefined;
+  };
+
+  var Header = Vue.extend({
     name: 'dev-header',
-    template: `
-    <header>
-      <img src="./${__BIOTOPE_DEV_PREVIEW_ROOT}/dev-preview.png"/>
-      <h1>biotope development server</h1>
-    </header>
-    `,
+    template: "\
+      <header>\
+        <img src=\"./" + __BIOTOPE_DEV_PREVIEW_ROOT + "/dev-preview.png\"/>\
+        <h1>biotope development server</h1>\
+      </header>\
+    ",
   });
   Vue.component('dev-header', Header);
 
-  const ComponentList = Vue.extend({
+  var ComponentList = Vue.extend({
     name: 'dev-component-list',
-    template: `
-      <ul>
-        <li v-for="route in routes"><router-link v-bind:to="route.path">{{route.name}}</router-link></li>
-      </ul>
-    `,
+    template: "\
+      <ul>\
+        <li v-for=\"route in routes\"><router-link v-bind:to=\"route.path\">{{route.name}}</router-link></li>\
+      </ul>\
+    ",
     computed: {
-      routes() {
+      routes: function() {
         return this.$router.options.routes;
       },
     },
   });
   Vue.component('dev-component-list', ComponentList);
 
-  const Overview = Vue.extend({
+  var Overview = Vue.extend({
     name: 'over-view',
-    template: `
-      <div class="overview">
-        <dev-header/>
-        <dev-component-list/>
-      </div>
-    `,
+    template: "\
+      <div class=\"overview\">\
+        <dev-header/>\
+        <dev-component-list/>\
+      </div>",
   });
   Vue.component('over-view', Overview);
 
-  const App = Vue.extend({
+  var App = Vue.extend({
     name: 'detail-view',
-    template: `
-      <div>
-        <over-view v-if="!$route.name"/>
-        <router-view></router-view>
-      </div>
-    `,
+    template: "\
+      <div>\
+        <over-view v-if=\"!$route.name\"/>\
+        <router-view></router-view>\
+      </div>\
+    ",
   });
-
   Vue.component('develop-preview', App);
 
-  const Detail = Vue.extend({
+  var Detail = Vue.extend({
     name: 'detail-view',
     template: '<div v-html="content">{{componentName}}</div>',
-    asyncComputed: {
-      async content() {
-        const url = this.$route.meta.fileUrl.replace('src/', '');
-        const html = await fetch(url).then((r) => r.text());
-        return html;
-      },
-    },
-    // updated: () => {
-    //   window.resourceLoader.update();
-    // },
     computed: {
-      componentName() {
+      componentName: function() {
         return this.$route.name;
+      },
+      content: function() {
+        var url = this.$route.meta.fileUrl.replace('src/', '');
+        return fetchText(url);
       },
     },
   });
 
-  const components = await fetch('components.json').then((r) => r.json());
-  const routes = components.map((component) => ({
-    path: `/${component.split('/').pop().replace(/\//g, '_')}`,
-    name: component.split('/').pop(),
-    component: Detail,
-    meta: { fileUrl: component },
-  }));
-  const router = new VueRouter({
-    routes,
+  var components = JSON.parse(fetchText('components.json'));
+  var routes = components.map(function(component) {
+    return {
+      path: '/' + component.split('/').pop().replace(/\//g, '_'),
+      name: component.split('/').pop(),
+      component: Detail,
+      meta: { fileUrl: component },
+    };
+  });
+  var router = new VueRouter({
+    routes: routes,
   });
 
   new Vue({
     el: '#app',
-    router,
+    router: router,
     data: {
-      routes,
+      routes: routes,
     },
   });
 })();
--- plugins/dev-preview/index.js
+++ plugins/dev-preview/index.js
@@ -14,7 +14,9 @@ const template = ({ output, prepend, append }) => `
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Development Preview</title>
+  <!-- Start: User Prepend Scripts -->
   ${prepend.reduce((acc, script) => `${acc}<script src="${script}"></script>`, '')}
+  <!-- End: User Prepend Scripts -->
 </head>
 
 <body>
@@ -22,14 +24,17 @@ const template = ({ output, prepend, append }) => `
     <develop-preview></develop-preview>
   </div>
 
-  <script src="https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.js"></script>
-  <script src="https://unpkg.com/vue-router/dist/vue-router.js"></script>
-  <script src="https://unpkg.com/vue-async-computed"></script>
-
-  <script>;__BIOTOPE_DEV_PREVIEW_ROOT="${output}";</script>
+  <!-- Start: Dev-Preview Plugin Style and Scripts -->
   <link rel="stylesheet" href="./${output}/dev-preview.css">
+  <script src="https://unpkg.com/vue@2.6.10/dist/vue.js"></script>
+  <script src="https://unpkg.com/vue-router@3.1.3/dist/vue-router.js"></script>
+  <script>;__BIOTOPE_DEV_PREVIEW_ROOT="${output}";</script>
   <script src="./${output}/dev-preview.js"></script>
+  <!-- End: Dev-Preview Plugin Style and Scripts -->
+
+  <!-- Start: User Append Scripts -->
   ${append.reduce((acc, script) => `${acc}<script src="${script}"></script>`, '')}
+  <!-- End: User Append Scripts -->
 </body>
 </html>
 `;
