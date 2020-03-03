7ba29a284736ee9c18ce83dcd0ef076dd09c825b
Tiago Marques <tiago.marques@virtual-identity.com>
2020-03-03 16:10:24 +0000

    Update projects

--- tests/fe-frameworks/src/index.js
+++ tests/fe-frameworks/src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Vue from 'vue';
-import loadEntry from 'load-entry';
 import { JsReactHello } from './components/js-react-hello';
 import { TsElementHello } from './components/ts-element-hello';
 import { TsReactHello } from './components/ts-react-hello';
 import { VueHello } from './components/vue-hello';
 import { TsVueHello } from './components/ts-vue-hello';
 
-loadEntry(() => {
-  TsElementHello.register();
-  document.querySelector(`#${IDS.TS_ELEMENT}`).innerHTML = '<ts-element-hello></ts-element-hello>';
-}, { event: 'WebComponentsReady' });
+TsElementHello.register();
+document.querySelector(`#${IDS.TS_ELEMENT}`).innerHTML = '<ts-element-hello></ts-element-hello>';
 
 ReactDOM.render(JsReactHello(), document.querySelector(`#${IDS.JS_REACT}`));
 ReactDOM.render(React.createElement(TsReactHello), document.querySelector(`#${IDS.TS_REACT}`));
